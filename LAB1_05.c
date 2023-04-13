1554330c6a6bc207edc1dd1b053fcbb96d07efc9
aa093094 <108619758+aa093094@users.noreply.github.com>
2023-04-13 09:25:36 +0900

    Update LAB1_05.c

--- LAB1_05.c
+++ LAB1_05.c
@@ -2,20 +2,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int bin_search(int list[], int search_num, int left, int right)
+int bin_search(int list[], int search_num, int left, int right, int size, int count)
 {
 	int middle = (left + right) / 2;
+	if (size == count)
+	{
+		return -1;
+	}
 		if (list[middle] == search_num)
 		{
 			return middle;
 		}
 		else if (list[middle] < search_num)
 		{
-			return bin_search(list, search_num, middle + 1, right);
+			count++;
+			return bin_search(list, search_num, middle + 1, right, size, count);
 		}
-		else
+		else if (list[middle] > search_num)
 		{
-			return bin_search(list, search_num, left, middle - 1);
+			count++;
+			return bin_search(list, search_num, left, middle - 1, size, count);
 		}
 
 	return -1;
@@ -35,7 +41,8 @@ int main()
 		arr_1[i] = k;
 	}
 	int left = 0, right = size - 1;
-	int result = bin_search(arr_1, num, left, right);
+	int count = 0;
+	int result = bin_search(arr_1, num, left, right, size, count);
 	fp2 = fopen("output.txt", "w");
 	fprintf(fp2, "%d", result);
 
